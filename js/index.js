bd81644bff29f08b2c6d825ee1c2be524c7ba3b5
carloseverton96 <140563844+carloseverton96@users.noreply.github.com>
2023-10-23 10:10:04 -0300

    23-10

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var elementosDuvidas = document.querySelectorAll('.duvida')
+
+elementosDuvidas.forEach(function (duvida) {
+    duvida.addEventListener('click', function(){
+        duvida.classList.toggle('ativa')
+    })
+})
+
+//
